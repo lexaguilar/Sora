f876d7b89644378f2dd99c72b3f9dac5fc1db3fd
Lex Aguilar <alejandro.aguilar@eurekani.com>
2020-06-16 14:57:56 -0600

    test variable

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ namespace sora
 {
     public class Startup
     {
+        private int i ;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
